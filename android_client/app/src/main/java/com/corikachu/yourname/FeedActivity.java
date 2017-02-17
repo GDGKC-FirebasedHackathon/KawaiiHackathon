0aca6c2ef4734632e1d7f8c8d47efde85a3c4af1
JuHong Jung <growingdever@gmail.com>
2017-02-18 00:11:14 +0900

    - MainActivity 변경   - FeedActivity로 변경   - FeedActivity에 floating action button 적용, 클릭하면 FeedDetailActivity로 이동 - FeedDetailActivity 생성

--- android_client/app/src/main/java/com/corikachu/yourname/FeedActivity.java
+++ android_client/app/src/main/java/com/corikachu/yourname/FeedActivity.java
@@ -1,19 +1,83 @@
 package com.corikachu.yourname;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-/**
- * Created by loki on 2017. 2. 17..
- */
+import com.corikachu.yourname.models.DTOFeed;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class FeedActivity extends AppCompatActivity {
 
+    @Bind(R.id.activity_main_recyclerview_feed_list)
+    RecyclerView feedListView;
+
+    @Bind(R.id.activity_main_fab_new_feed)
+    FloatingActionButton newFeedButton;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    private FeedAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
 
+        ButterKnife.bind(this);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        layoutManager.scrollToPosition(0);
+        feedListView.setLayoutManager(layoutManager);
+
+        adapter = new FeedAdapter(new ArrayList<DTOFeed>(), this);
+        feedListView.setAdapter(adapter);
+
+        databaseReference.child("feeds").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                DTOFeed feed = dataSnapshot.getValue(DTOFeed.class);
+                adapter.addItem(feed);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                // TODO : implement it
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
+    }
+
+    @OnClick(R.id.activity_main_fab_new_feed)
+    public void onClickNewFeed(View view) {
+        Intent intent = new Intent(this, NewFeedActivity.class);
+        this.startActivity(intent);
     }
 
 }
