7766443b096f9ac1fb1149033dc06a2ab208e8fa
Corikachu <konomama0505@gmail.com>
2017-02-18 06:19:10 +0900

    fix AuthActivity 잘못된 import

--- android_client/app/src/main/java/com/corikachu/yourname/AuthActivity.java
+++ android_client/app/src/main/java/com/corikachu/yourname/AuthActivity.java
@@ -1,30 +1,11 @@
 package com.corikachu.yourname;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Default File Header.
@@ -34,10 +15,6 @@ import butterknife.OnClick;
 public class AuthActivity extends AppCompatActivity {
 
     private static final String TAG = AuthActivity.class.getName();
-
-
-    @Bind(R.id.sign_in_button)
-    Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
